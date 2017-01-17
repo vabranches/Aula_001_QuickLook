0ae555d078a2f0d2cecf1126ad512422d4b7786f
Swift <swift@WozMac8.local>
2017-01-16 22:05:21 -0200

    Inicio

--- Aula_001_QuickLook/ViewController.swift
+++ Aula_001_QuickLook/ViewController.swift
@@ -1,24 +1,41 @@
-//
-//  ViewController.swift
-//  Aula_001_QuickLook
-//
-//  Created by Swift on 16/01/17.
-//
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+//Importa o framework QuickLook
+import QuickLook
+
+class ViewController: UIViewController, QLPreviewControllerDataSource{
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: Action
+    @IBAction func mostrarArquivo(_ sender: UIButton) {
+        
+        let quickLook = QLPreviewController()
+        quickLook.dataSource = self
+        
+        //Empurrando o objteto para a sequencia do navigation controller
+        self.navigationController?.pushViewController(quickLook, animated: true)
     }
+    
+    //MARK: DataSource
+    func numberOfPreviewItems(in controller: QLPreviewController) -> Int {
+        return 1
+    }
+    
+    func previewController(_ controller: QLPreviewController, previewItemAt index: Int) -> QLPreviewItem {
+        
+        let nomeArquivo = "conselhos"
+        let extensao = "pdf"
+        let raizDoApp = Bundle.main
+        let caminhoArquivo = raizDoApp.path(forResource: nomeArquivo, ofType: extensao)
+        let urlArquivo = URL(fileURLWithPath: caminhoArquivo!)
+        return urlArquivo as QLPreviewItem
+    }
+
 
 
 }
